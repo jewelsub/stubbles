e360218462248f70ae3cd89cd12887020e43c2d7
A.K.M. Ashrafuzzaman <jitu@dragon.(none)>
2012-01-17 23:06:40 +0600

    Added jquery ui bubbles in the stories

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,32 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require jquery-ui
+
+
+//adding the sortability in the sortable_column
+// as well as adding the appropriate classes for the widget
+(function( $ ){
+  $.fn.addPortletClassesToDom = function() {
+    this.addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+		.find( ".portlet-header" )
+			.addClass( "ui-widget-header ui-corner-all" )
+			.prepend( "<span class='ui-icon ui-icon-minusthick'></span>")
+			.end()
+		.find( ".portlet-content" );
+
+	$( this, ".portlet-header .ui-icon" ).click(function() {
+		$( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
+		$( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
+	});
+	return this;
+  };
+})( jQuery );
+
+$(function() {
+	$( ".sortable_column" ).sortable({
+		connectWith: ".column"
+	});
+	$( ".portlet" ).addPortletClassesToDom ();
+	$( ".sortable_column" ).disableSelection();
+});
