c721c9e6db8bd2dbd1a6e983ed0ef8786fb2d34b
A.K.M. Ashrafuzzaman <jitu@dragon.(none)>
2012-01-20 21:17:05 +0600

    Merged with master

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,33 @@ module ApplicationHelper
       yield
     puts %w(</div>)
   end
+  def box_container(containerId)
+  	concat <<-EOF.html_safe
+    	<div id='#{containerId}' class="portlet ui-widget ui-widget-content ui-helper-clearfix ui-corner-all">
+  	EOF
+	yield
+  	concat <<-EOF.html_safe
+    	</div>
+  	EOF
+  end
+
+  def box_header()
+  	concat <<-EOF.html_safe
+    	<div class="portlet-header ui-widget-header ui-corner-all"><span class="ui-icon ui-icon-minusthick"></span>
+  	EOF
+	yield
+  	concat <<-EOF.html_safe
+    	</div>
+  	EOF
+  end
+
+  def box_content()
+  	concat <<-EOF.html_safe
+    	<div class="portlet-content" style="display: block; ">
+  	EOF
+	yield
+  	concat <<-EOF.html_safe
+    	</div>
+  	EOF
+  end
 end
